f7c52dbbf866dfdb7563321ef4c192599f8509f0
Greg Sweetman <greg.sweetman@zpg.co.uk>
2018-06-14 11:13:58 +0100

    Adding an alias to quickly switch go->code->go->

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -13,3 +13,6 @@ alias sublh='subl -n .'
 
 # Do updates and upgrades for installed packages.
 alias sapt='sudo apt-get update && sudo apt-get --with-new-pkgs upgrade'
+
+# Toggle alias to jump between go verion of a repo and the code version.
+alias gogo='GOTO=$(pwd | sed "s:$HOME/code/zpg:$HOME/~~/go/src/github.com/uswitch:; s:$HOME/go/src/github.com/uswitch:$HOME/code/zpg:; s:$HOME/~~/:$HOME/:") ; cd $GOTO ; echo -e "Changed to $(colourizePath -d 3 $GOTO)"'
