bce358be0bb2d1674f6106c2f7773134b954cb99
Greg Sweetman <greg.sweetman@zpg.co.uk>
2018-06-28 07:33:12 +0100

    Adding some aliases for kubectl and git.

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -16,3 +16,16 @@ alias sapt='sudo apt-get update && sudo apt-get --with-new-pkgs upgrade'
 
 # Toggle alias to jump between go verion of a repo and the code version.
 alias gogo='GOTO=$(pwd | sed "s:$HOME/code/zpg:$HOME/~~/go/src/github.com/uswitch:; s:$HOME/go/src/github.com/uswitch:$HOME/code/zpg:; s:$HOME/~~/:$HOME/:") ; cd $GOTO ; echo -e "Changed to $(colourizePath -d 3 $GOTO)"'
+
+# Kubectl
+alias k='kubectl'
+alias kl='kubectl logs'
+
+# Git
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gcm='git checkout master'
+alias gs='git status'
+alias ga='git add'
+alias gr='git rm'
+alias gc='git commit'
